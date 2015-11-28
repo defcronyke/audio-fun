222eba114d332d5a381ca90bff1e5b5370de3605
Jeremy Carter <Jeremy@JeremyCarter.ca>
2015-11-28 12:54:16 -0500

    Jeremy Equalizer 1 finished

--- src/app/components/visualization/visualizations/EqualizerJeremy1/EqualizerJeremy1.directive.js
+++ src/app/components/visualization/visualizations/EqualizerJeremy1/EqualizerJeremy1.directive.js
@@ -26,6 +26,9 @@ class EqualizerJeremy1Controller {
 		
 		if (selectedVisual.name === 'equalizer-jeremy1') {
 		
+			this.canvasWidth = 1024;
+			this.canvasHeight = 768;
+		
 			this.audioPlayer = angular.element('#audio-player')[0];
 			
 			let AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -37,14 +40,23 @@ class EqualizerJeremy1Controller {
 			this.audioAnalyser.fftSize = 2048;
 			this.bufferLength = this.audioAnalyser.frequencyBinCount;
 	
-	//		this.dataArray = new Uint8Array(this.bufferLength);
-			this.dataFloatArray = new Float32Array(this.bufferLength);
+			this.dataArray = new Uint8Array(this.bufferLength);
+//			this.dataFloatArray = new Float32Array(this.bufferLength);
 			
-	//		this.audioAnalyser.getByteTimeDomainData(this.dataArray);
-			this.audioAnalyser.getFloatTimeDomainData(this.dataFloatArray);
+			this.audioAnalyser.getByteTimeDomainData(this.dataArray);
+//			this.audioAnalyser.getFloatTimeDomainData(this.dataFloatArray);
 			
 			this.audioSource.connect(this.audioAnalyser);
 			this.audioAnalyser.connect(this.audioContext.destination);
+			
+			
+			
+			this.canvas = angular.element('#equalizer-jeremy1-canvas1')[0];
+			this.canvasContext = this.canvas.getContext('2d');
+			this.canvasContext.fillStyle = 'rgba(0, 0, 0, 1.0)';
+			this.canvasContext.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+			
+			
 			
 			this.processAudio();
 		}
@@ -70,22 +82,62 @@ class EqualizerJeremy1Controller {
 		
 		this.processAudioHandle = requestAnimationFrame(this.processAudio.bind(this));
 		
-//		this.audioAnalyser.getByteTimeDomainData(this.dataArray)
-		this.audioAnalyser.getFloatTimeDomainData(this.dataFloatArray)
+		this.audioAnalyser.getByteTimeDomainData(this.dataArray)
+//		this.audioAnalyser.getFloatTimeDomainData(this.dataFloatArray)
 		
 		let skip = true;
 		
 		for (let i = 0; i < this.bufferLength; i++) {
 			
-			let data = this.dataFloatArray[i];
+			let data = this.dataArray[i];
+//			let data = this.dataFloatArray[i];
 			
-			if (data !== 0.0) {
+//			if (data !== 0.0) {
+//				skip = false;
+//			}
+
+			if (data !== 128) {
 				skip = false;
 			}
 		}
 		
 		if (!skip) {
-			console.log(this.dataFloatArray);
+			
+//			console.log(this.dataArray);
+			this.draw(this.dataArray);
+			
+//			console.log(this.dataFloatArray);
+//			this.draw(this.dataFloatArray);
+		}
+	}
+	
+	draw(dataArray) {
+
+		let rand_r = Math.floor(Math.random() * 255);
+		let rand_g = Math.floor(Math.random() * 255);
+		let rand_b = Math.floor(Math.random() * 255);
+
+		//this.canvasContext.fillStyle = 'rgba(0, 0, 0, 1.0)';
+		
+		this.canvasContext.fillStyle = 'rgba('+dataArray[rand_r]+', '+dataArray[rand_g]+', '+dataArray[rand_b]+', 1.0)';
+		
+		this.canvasContext.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+
+		this.canvasContext.fillStyle = 'rgba(255, 0, 0, 1.0)';
+
+		for (let i = 0; i < dataArray.length; i++) {
+			
+			let data = dataArray[i];
+
+			let rand_r2 = Math.floor(Math.random() * 255);
+			let rand_g2 = Math.floor(Math.random() * 255);
+			let rand_b2 = Math.floor(Math.random() * 255);
+
+			this.canvasContext.fillStyle = 'rgba('+rand_r2+', 0, '+rand_b2+', 1.0)';
+			this.canvasContext.fillRect(i, 0, 1, data);
+			
+			this.canvasContext.fillStyle = 'rgba(0, '+rand_g2+', '+rand_b2+', 1.0)';
+			this.canvasContext.fillRect(i, this.canvasHeight, 1, -data);
 		}
 	}
 }
